840345a9737905048fbf6994215bd2e1dbc63da7
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2023-04-27 10:59:44 +0700

    MS-8: connected SonarQube to project

---
+++ MidpointSonar/src/main/java/ru/nsu/fit/g20203/sdwm/midpointsonar/Main.java
@@ -0,0 +1,8 @@
+package ru.nsu.fit.g20203.sdwm.midpointsonar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from MidPoint Sonar!");
+    }
+}
