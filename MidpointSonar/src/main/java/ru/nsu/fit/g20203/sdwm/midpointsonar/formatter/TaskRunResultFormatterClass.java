64c3170e2ffb74a84ab2e6efa18b84e64d7f4bbe
Darya <d.novikova2@g.nsu.ru>
2023-05-03 22:45:54 +0700

    MS-31: TaskRunResultFormatter realization and tests are done

--- MidpointSonar/src/main/java/ru/nsu/fit/g20203/sdwm/midpointsonar/formatter/TaskRunResultFormatterClass.java
+++ MidpointSonar/src/main/java/ru/nsu/fit/g20203/sdwm/midpointsonar/formatter/TaskRunResultFormatterClass.java
@@ -1,2 +1,112 @@
-package ru.nsu.fit.g20203.sdwm.midpointsonar.formatter;public class TaskRunResultFormatterClass {
+package ru.nsu.fit.g20203.sdwm.midpointsonar.formatter;
+
+import ru.nsu.fit.g20203.sdwm.midpointsonar.midpoint.MidPointSonarObject;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.Status;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.async.QPRunResult;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.async.RuleLoadResult;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.async.RuleRunResult;
+
+import java.util.concurrent.ExecutionException;
+
+import static ru.nsu.fit.g20203.sdwm.midpointsonar.result.async.RuleLoadResult.RuleLoadStatus.*;
+import static ru.nsu.fit.g20203.sdwm.midpointsonar.result.sync.QPOperationResult.QPOperationStatus.NO_SUCH_QUALITY_PROFILE;
+import static ru.nsu.fit.g20203.sdwm.midpointsonar.result.sync.RuleOperationResult.RuleOperationStatus.NO_SUCH_RULE;
+import static ru.nsu.fit.g20203.sdwm.midpointsonar.result.Status.SUCCESS;
+
+public abstract class TaskRunResultFormatterClass implements TaskRunResultFormatter {
+    public String formatRuleLoadResult(RuleLoadResult ruleLoadResult) {
+        StringBuilder formattedRuleLoadResult = new StringBuilder();
+
+        if (!ruleLoadResult.getRuleLoadStatus().isDone()) {
+            formattedRuleLoadResult.append("Rule with name " +
+                    ruleLoadResult.getRuleOperationResult().getRule().getName() +
+                    "hasn't loaded yet. ");
+        } else if (ruleLoadResult.getRuleOperationResult().getStatus() == NO_SUCH_RULE) {
+            formattedRuleLoadResult.append("There is no rule with name " +
+                                            ruleLoadResult.getRuleOperationResult().getRule().getName() + ". ");
+        } else {
+            formattedRuleLoadResult.append("Rule named "
+                                            + ruleLoadResult.getRuleOperationResult().getRule().getName() +
+                                            " with server task " +
+                                            ruleLoadResult.getRuleOperationResult().getRule().getServerTask());
+
+            RuleLoadResult.RuleLoadStatus ruleLoadStatus = null;
+            try {
+                ruleLoadStatus = ruleLoadResult.getRuleLoadStatus().get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+
+            switch (ruleLoadStatus) {
+                case NOT_NEEDED:
+                    formattedRuleLoadResult.append(" doesn't need to be loaded. ");
+                    break;
+                case SUCCESS:
+                    formattedRuleLoadResult.append(" loaded successfully. ");
+                    break;
+                case ERROR:
+                    formattedRuleLoadResult.append(" failed to load. ");
+                    break;
+            }
+        }
+
+        return formattedRuleLoadResult.toString();
+    };
+
+    public String formatRuleRunResult(RuleRunResult ruleRunResult){
+        StringBuilder formattedRuleRunResult = new StringBuilder();
+
+        formattedRuleRunResult.append(formatRuleLoadResult(ruleRunResult.getRuleLoadResult()));
+
+        if (ruleRunResult.getRuleOperationResult().getStatus() == NO_SUCH_RULE) {
+            formattedRuleRunResult.append("Cannot be run. \n");
+        } else {
+            formattedRuleRunResult.append("Rule named "
+                    + ruleRunResult.getRuleOperationResult().getRule().getName() +
+                    " with server task " +
+                    ruleRunResult.getRuleOperationResult().getRule().getServerTask());
+
+            Status ruleRunStatus = null;
+            try {
+                ruleRunStatus = ruleRunResult.getRuleRunStatus().get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            if (ruleRunStatus.equals(SUCCESS)) {
+                formattedRuleRunResult.append(" ran successfully. \n");
+            } else {
+                formattedRuleRunResult.append(" failed to run. \n");
+            }
+
+            /*try {
+                for (MidPointSonarObject midPointSonarObject: ruleRunResult.getObjects().get()) {
+                    formatMidPointSonarObject(midPointSonarObject);
+                }
+            } catch (InterruptedException | ExecutionException ignore) {
+
+            }*/
+        }
+
+        return formattedRuleRunResult.toString();
+    }
+
+    public String formatQPRunResult(QPRunResult qpRunResult) {
+        StringBuilder formattedQPRunResult = new StringBuilder();
+
+        if (qpRunResult.getQpOperationResult().getStatus() == NO_SUCH_QUALITY_PROFILE) {
+            formattedQPRunResult.append("There is no Quality Profile with name " +
+                                        qpRunResult.getQpOperationResult().getQualityProfile().getName() + ". \n");
+        } else {
+            formattedQPRunResult.append("Run results for Rules ih Quality Profile named " +
+                                        qpRunResult.getQpOperationResult().getQualityProfile().getName() + ": \n");
+            for (RuleRunResult ruleRunResult: qpRunResult.getRuleRunResults()) {
+                formattedQPRunResult.append(formatRuleRunResult(ruleRunResult));
+            }
+        }
+
+        return formattedQPRunResult.toString();
+    };
+
+    public abstract String formatMidPointSonarObject (MidPointSonarObject midPointSonarObject);
 }
--- MidpointSonar/src/test/java/ru/nsu/fit/g20203/sdwm/midpointsonar/formatter/TaskRunResultFormatterClassTest.java
+++ MidpointSonar/src/test/java/ru/nsu/fit/g20203/sdwm/midpointsonar/formatter/TaskRunResultFormatterClassTest.java
@@ -1,4 +1,194 @@
+package ru.nsu.fit.g20203.sdwm.midpointsonar.formatter;
+
+import org.junit.jupiter.api.Test;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.midpoint.MidPointSonarObject;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.midpoint.ServerTask;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.qualityprofile.QualityProfile;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.qualityprofile.Rule;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.Status;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.async.QPRunResult;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.async.RuleLoadResult;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.async.RuleRunResult;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.sync.QPOperationResult;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.sync.RuleOperationResult;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static ru.nsu.fit.g20203.sdwm.midpointsonar.result.async.RuleLoadResult.RuleLoadStatus.ERROR;
+import static ru.nsu.fit.g20203.sdwm.midpointsonar.result.async.RuleLoadResult.RuleLoadStatus.SUCCESS;
+
 class TaskRunResultFormatterClassTest {
-  
+
+    @Test
+    void formatRuleLoadResultNoSuch() {
+        Rule rule = new Rule() {
+            @Override
+            public ServerTask getServerTask() {
+                return null;
+            }
+        };
+        rule.setName("Name");
+        RuleOperationResult ruleOperationResult = new RuleOperationResult(RuleOperationResult.RuleOperationStatus.NO_SUCH_RULE, rule);
+        Future<RuleLoadResult.RuleLoadStatus> ruleLoadStatus = CompletableFuture.completedFuture(ERROR);
+        RuleLoadResult ruleLoadResult = new RuleLoadResult(Long.valueOf(1), ruleLoadStatus);
+        TaskRunResultFormatterClass taskRunResultFormatterClass = new TaskRunResultFormatterClass() {
+            @Override
+            public String formatMidPointSonarObject(MidPointSonarObject midPointSonarObject) {
+                return null;
+            }
+        };
+        ruleLoadResult.setRuleOperationResult(ruleOperationResult);
+
+        String expected = "There is no rule with name Name. ";
+        String actual = taskRunResultFormatterClass.formatRuleLoadResult(ruleLoadResult);
+
+        assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    void formatRuleLoadResultSuccess() throws ExecutionException, InterruptedException {
+        Rule rule = new Rule() {
+            @Override
+            public ServerTask getServerTask() {
+                return null;
+            }
+        };
+        rule.setName("Name");
+        RuleOperationResult ruleOperationResult = new RuleOperationResult(RuleOperationResult.RuleOperationStatus.SUCCESS, rule);
+        Future<RuleLoadResult.RuleLoadStatus> ruleLoadStatus = CompletableFuture.completedFuture(SUCCESS);
+        RuleLoadResult ruleLoadResult = new RuleLoadResult(Long.valueOf(1), ruleLoadStatus);
+        TaskRunResultFormatterClass taskRunResultFormatterClass = new TaskRunResultFormatterClass() {
+            @Override
+            public String formatMidPointSonarObject(MidPointSonarObject midPointSonarObject) {
+                return null;
+            }
+        };
+        ruleLoadResult.setRuleOperationResult(ruleOperationResult);
+
+        String expected = "Rule named Name with server task null loaded successfully. ";
+        String actual = taskRunResultFormatterClass.formatRuleLoadResult(ruleLoadResult);
+
+        assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    void formatRuleRunResultNoSuch() {
+        Rule rule = new Rule() {
+            @Override
+            public ServerTask getServerTask() {
+                return null;
+            }
+        };
+        rule.setName("Name");
+        RuleOperationResult ruleOperationResult = new RuleOperationResult(RuleOperationResult.RuleOperationStatus.NO_SUCH_RULE, rule);
+        Future<RuleLoadResult.RuleLoadStatus> ruleLoadStatus = CompletableFuture.completedFuture(SUCCESS);
+        RuleLoadResult ruleLoadResult = new RuleLoadResult(Long.valueOf(1), ruleLoadStatus);
+        TaskRunResultFormatterClass taskRunResultFormatterClass = new TaskRunResultFormatterClass() {
+            @Override
+            public String formatMidPointSonarObject(MidPointSonarObject midPointSonarObject) {
+                return null;
+            }
+        };
+        ruleLoadResult.setRuleOperationResult(ruleOperationResult);
+        Future<Status> ruleRunStatus = CompletableFuture.completedFuture(Status.SUCCESS);
+        Collection<MidPointSonarObject> midPointSonarObjectCollection = null;
+        Future<Collection<MidPointSonarObject>> objects = CompletableFuture.completedFuture(midPointSonarObjectCollection);
+        RuleRunResult ruleRunResult = new RuleRunResult(Long.valueOf(1), ruleLoadResult, ruleRunStatus, objects);
+        ruleRunResult.setRuleOperationResult(ruleOperationResult);
+
+        String expected = "There is no rule with name Name. Cannot be run. \n";
+        String actual = taskRunResultFormatterClass.formatRuleRunResult(ruleRunResult);
+
+        assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    void formatRuleRunResultSuccess() {
+        Rule rule = new Rule() {
+            @Override
+            public ServerTask getServerTask() {
+                return null;
+            }
+        };
+        rule.setName("Name");
+        RuleOperationResult ruleOperationResult = new RuleOperationResult(RuleOperationResult.RuleOperationStatus.SUCCESS, rule);
+        Future<RuleLoadResult.RuleLoadStatus> ruleLoadStatus = CompletableFuture.completedFuture(SUCCESS);
+        RuleLoadResult ruleLoadResult = new RuleLoadResult(Long.valueOf(1), ruleLoadStatus);
+        TaskRunResultFormatterClass taskRunResultFormatterClass = new TaskRunResultFormatterClass() {
+            @Override
+            public String formatMidPointSonarObject(MidPointSonarObject midPointSonarObject) {
+                return null;
+            }
+        };
+        ruleLoadResult.setRuleOperationResult(ruleOperationResult);
+        Future<Status> ruleRunStatus = CompletableFuture.completedFuture(Status.SUCCESS);
+        Collection<MidPointSonarObject> midPointSonarObjectCollection = null;
+        Future<Collection<MidPointSonarObject>> objects = CompletableFuture.completedFuture(midPointSonarObjectCollection);
+        RuleRunResult ruleRunResult = new RuleRunResult(Long.valueOf(1), ruleLoadResult, ruleRunStatus, objects);
+        ruleRunResult.setRuleOperationResult(ruleOperationResult);
+
+        String expected = "Rule named Name with server task null loaded successfully. Rule named Name with server task null ran successfully. \n";
+        String actual = taskRunResultFormatterClass.formatRuleRunResult(ruleRunResult);
+
+        assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    void formatQPRunResultSuccess() {
+        Rule rule = new Rule() {
+            @Override
+            public ServerTask getServerTask() {
+                return null;
+            }
+        };
+        rule.setName("Name");
+        RuleOperationResult ruleOperationResult = new RuleOperationResult(RuleOperationResult.RuleOperationStatus.SUCCESS, rule);
+        Future<RuleLoadResult.RuleLoadStatus> ruleLoadStatus = CompletableFuture.completedFuture(SUCCESS);
+        RuleLoadResult ruleLoadResult = new RuleLoadResult(Long.valueOf(1), ruleLoadStatus);
+        TaskRunResultFormatterClass taskRunResultFormatterClass = new TaskRunResultFormatterClass() {
+            @Override
+            public String formatMidPointSonarObject(MidPointSonarObject midPointSonarObject) {
+                return null;
+            }
+        };
+        ruleLoadResult.setRuleOperationResult(ruleOperationResult);
+        Future<Status> ruleRunStatus = CompletableFuture.completedFuture(Status.SUCCESS);
+        Collection<MidPointSonarObject> midPointSonarObjectCollection = null;
+        Future<Collection<MidPointSonarObject>> objects = CompletableFuture.completedFuture(midPointSonarObjectCollection);
+        RuleRunResult ruleRunResult = new RuleRunResult(Long.valueOf(1), ruleLoadResult, ruleRunStatus, objects);
+        ruleRunResult.setRuleOperationResult(ruleOperationResult);
+        List<RuleRunResult> ruleRunResults = new ArrayList<>() {{add(ruleRunResult);}};
+        QualityProfile qualityProfile = new QualityProfile() {
+            @Override
+            public QPOperationResult addRule(Rule rule) {
+                return null;
+            }
+
+            @Override
+            public QPOperationResult removeRule(String ruleName) {
+                return null;
+            }
+
+            @Override
+            public Collection<Rule> getAllRules() {
+                return null;
+            }
+        };
+        QPOperationResult qpOperationResult = new QPOperationResult(QPOperationResult.QPOperationStatus.SUCCESS, qualityProfile);
+        QPRunResult qpRunResult = new QPRunResult(Long.valueOf(1), ruleRunResults, qpOperationResult);
+
+        String expected = "Run results for Rules ih Quality Profile named null: \n" +
+                            "Rule named Name with server task null loaded successfully. " +
+                            "Rule named Name with server task null ran successfully. \n";
+        String actual = taskRunResultFormatterClass.formatQPRunResult(qpRunResult);
+
+        assertTrue(actual.equals(expected));
+    }
 }
