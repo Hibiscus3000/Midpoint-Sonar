48de12dbe505eec3ffa1be2740dcc7562710f5b3
Nikomar7 <n.butkhuzi@g.nsu.ru>
2023-05-04 17:28:25 +0600

    MS-34: Add repositories

---
+++ MidpointSonar/src/main/java/ru/nsu/fit/g20203/sdwm/midpointsonar/qualityprofile/Repositories/QPRepository.java
@@ -0,0 +1,9 @@
+package ru.nsu.fit.g20203.sdwm.midpointsonar.qualityprofile.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.qualityprofile.QualityProfile;
+
+public interface QPRepository extends JpaRepository<QualityProfile,Long> {
+    QualityProfile findByName(String name);
+
+}
