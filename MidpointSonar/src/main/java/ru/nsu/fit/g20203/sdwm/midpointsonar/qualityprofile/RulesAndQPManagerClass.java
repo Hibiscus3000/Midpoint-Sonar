355419ee91880a8870525c81ae6cbc2a961c038c
Nikomar7 <n.butkhuzi@g.nsu.ru>
2023-05-04 17:58:55 +0600

    MS-34: Strange Implementation of add and remove ruleProfile

--- MidpointSonar/src/main/java/ru/nsu/fit/g20203/sdwm/midpointsonar/qualityprofile/RulesAndQPManagerClass.java
+++ MidpointSonar/src/main/java/ru/nsu/fit/g20203/sdwm/midpointsonar/qualityprofile/RulesAndQPManagerClass.java
@@ -5,10 +5,12 @@ import ru.nsu.fit.g20203.sdwm.midpointsonar.qualityprofile.Repositories.QPReposi
 import ru.nsu.fit.g20203.sdwm.midpointsonar.qualityprofile.Repositories.RuleRepository;
 import ru.nsu.fit.g20203.sdwm.midpointsonar.result.sync.QPAndRuleOperationResult;
 import ru.nsu.fit.g20203.sdwm.midpointsonar.result.sync.QPOperationResult;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.result.sync.RuleOperationResult;
 
 import java.util.Collection;
 
 import static ru.nsu.fit.g20203.sdwm.midpointsonar.result.sync.QPOperationResult.QPOperationStatus.NO_SUCH_QUALITY_PROFILE;
+import static ru.nsu.fit.g20203.sdwm.midpointsonar.result.sync.QPOperationResult.QPOperationStatus.SUCCESS;
 
 public class RulesAndQPManagerClass implements RulesAndQPManager {
     private final QPRepository qpRepository;
@@ -20,12 +22,27 @@ public class RulesAndQPManagerClass implements RulesAndQPManager {
     }
     @Override
     public QPOperationResult createNewQualityProfile(String profileName) {
-        return null;
+        if (qpRepository.findByName(profileName) != null) {
+            return new QPOperationResult(QPOperationResult.QPOperationStatus.QP_WITH_GIVEN_NAME_ALREADY_EXISTS, null);
+        }
+        QualityProfile qp = new QualityProfile(profileName);
+        qpRepository.save(qp);
+        return new QPOperationResult(QPOperationResult.QPOperationStatus.SUCCESS, qp);
     }
     @Override
     public QPOperationResult renameQualityProfile(String oldName, String newName) {
-        return null;
+        QualityProfile qp = qpRepository.findByName(oldName);
+        if (qp == null){
+            return new QPOperationResult(NO_SUCH_QUALITY_PROFILE,null);
+        }
+        qp.setName(newName);
+        if (qpRepository.findByName(newName) != null) {
+            return new QPOperationResult(QPOperationResult.QPOperationStatus.QP_WITH_GIVEN_NAME_ALREADY_EXISTS, null);
+        }
+        qpRepository.save(qp);
+        return new QPOperationResult(QPOperationResult.QPOperationStatus.SUCCESS, qp);
     }
+
 
     @Override
     public QPOperationResult removeQualityProfile(String profileName) {
@@ -39,12 +56,66 @@ public class RulesAndQPManagerClass implements RulesAndQPManager {
 
     @Override
     public QPAndRuleOperationResult addRuleToQualityProfile(String ruleName, String profileName) {
-        return null;
+        QualityProfile qp =  qpRepository.findByName(profileName);
+        if (qp == null){
+            return new QPAndRuleOperationResult(QPAndRuleOperationResult.QPAndRuleOperationStatus.SUCCESS,
+                    new QPOperationResult(NO_SUCH_QUALITY_PROFILE,null),
+                    new RuleOperationResult(RuleOperationResult.RuleOperationStatus.SUCCESS,
+                            new QualityProfile(profileName)));
+        }
+        Rule rule = ruleRepository.findByName(ruleName);
+        if (rule == null){
+            return new QPAndRuleOperationResult(QPAndRuleOperationResult.QPAndRuleOperationStatus.SUCCESS,
+                    new QPOperationResult(QPOperationResult.QPOperationStatus.SUCCESS,qp),
+                    new RuleOperationResult(RuleOperationResult.RuleOperationStatus.NO_SUCH_RULE,
+                            new Rule(ruleName)));
+        }
+        QPOperationResult qpr = qp.addRule(rule);
+        if (qpr.getStatus() == SUCCESS){
+            qpRepository.save(qp);
+            return new QPAndRuleOperationResult(QPAndRuleOperationResult.QPAndRuleOperationStatus.SUCCESS,
+                    new QPOperationResult(QPOperationResult.QPOperationStatus.SUCCESS,qp),
+                    new RuleOperationResult(RuleOperationResult.RuleOperationStatus.SUCCESS,rule));
+        }
+        else {
+            qpRepository.save(qp);
+            return new QPAndRuleOperationResult(QPAndRuleOperationResult.QPAndRuleOperationStatus.RULE_ALREADY_IN_QP,
+                    new QPOperationResult(QPOperationResult.QPOperationStatus.SUCCESS, qp),
+                    new RuleOperationResult(RuleOperationResult.RuleOperationStatus.SUCCESS, rule));
+        }
+
     }
 
     @Override
     public QPAndRuleOperationResult removeRuleFromQualityProfile(String ruleName, String profileName) {
-        return null;
+        QualityProfile qp =  qpRepository.findByName(profileName);
+        if (qp == null) {
+            return new QPAndRuleOperationResult(QPAndRuleOperationResult.QPAndRuleOperationStatus.SUCCESS,
+                    new QPOperationResult(NO_SUCH_QUALITY_PROFILE, null),
+                    new RuleOperationResult(RuleOperationResult.RuleOperationStatus.SUCCESS,
+                            new QualityProfile(profileName)));
+        }
+        Rule rule = ruleRepository.findByName(ruleName);
+        if (rule == null) {
+            return new QPAndRuleOperationResult(QPAndRuleOperationResult.QPAndRuleOperationStatus.SUCCESS,
+                    new QPOperationResult(QPOperationResult.QPOperationStatus.SUCCESS, qp),
+                    new RuleOperationResult(RuleOperationResult.RuleOperationStatus.NO_SUCH_RULE,
+                            new Rule(ruleName)));
+        }
+        QPOperationResult qpr = qp.removeRule(ruleName);
+        if (qpr.getStatus() == SUCCESS){
+            qpRepository.save(qp);
+            return new QPAndRuleOperationResult(QPAndRuleOperationResult.QPAndRuleOperationStatus.SUCCESS,
+                    new QPOperationResult(QPOperationResult.QPOperationStatus.SUCCESS,qp),
+                    new RuleOperationResult(RuleOperationResult.RuleOperationStatus.SUCCESS,rule));
+        }
+        else {
+            qpRepository.save(qp);
+            return new QPAndRuleOperationResult(QPAndRuleOperationResult.QPAndRuleOperationStatus.RULE_NOT_IN_QP,
+                    new QPOperationResult(QPOperationResult.QPOperationStatus.SUCCESS, qp),
+                    new RuleOperationResult(RuleOperationResult.RuleOperationStatus.SUCCESS, rule));
+        }
+
     }
 
     @Override
