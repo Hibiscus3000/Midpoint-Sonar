16793e7b424ced833f0f51e4c4b2ab73ab196f5e
Nikomar7 <n.butkhuzi@g.nsu.ru>
2023-05-25 09:26:27 +0600

    MS-47: test from

---
+++ MidpointSonar/src/main/java/ru/nsu/fit/g20203/sdwm/midpointsonar/controller/LoginController.java
@@ -0,0 +1,46 @@
+package ru.nsu.fit.g20203.sdwm.midpointsonar.controller;
+
+import com.evolveum.midpoint.client.api.exception.CommonException;
+import com.evolveum.midpoint.client.api.exception.SchemaException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.entity.ConnectionData;
+import ru.nsu.fit.g20203.sdwm.midpointsonar.midpoint.MidPoint;
+
+import java.io.FileNotFoundException;
+
+
+
+@Controller
+class LoginController {
+    @GetMapping("/login")
+    public String login(Model model) {
+        ConnectionData conn = new ConnectionData();
+        conn.setURL("http://localhost:8080/midpoint/ws/rest");
+        conn.setLogin("administrator");
+        conn.setPassword("5ecr3t");
+        model.addAttribute("connectionData", conn);
+
+
+        return "login";
+    }
+    /*
+    Отсюда сохранить данные в нужные поля теста для создания соединения?
+     */
+    @PostMapping("/login")
+    public String greetingSubmit(@ModelAttribute ConnectionData conn, Model model)  {
+        try {
+            MidPoint.init(conn.getURL(), conn.getLogin(), conn.getPassword());
+        }
+        catch (FileNotFoundException | SchemaException e) {
+            e.printStackTrace();
+            model.addAttribute("loginError", true);
+            return "login";
+        }
+        model.addAttribute("loginSuccess", true);
+        return "login";
+    }
+}
---
+++ MidpointSonar/src/main/java/ru/nsu/fit/g20203/sdwm/midpointsonar/entity/ConnectionData.java
@@ -0,0 +1,17 @@
+package ru.nsu.fit.g20203.sdwm.midpointsonar.entity;
+
+import lombok.Data;
+
+@Data
+public class ConnectionData {
+
+
+    private String URL;
+
+
+    private String password;
+
+
+    private String login;
+
+}
